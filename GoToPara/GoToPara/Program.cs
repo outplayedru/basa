03bb9ae0269c0c92b93b93a9d4d27b748aa65593
outplayedru <malofeevs00@gmail.com>
2021-11-10 10:26:04 +0300

    basa2

--- GoToPara/GoToPara/Program.cs
+++ GoToPara/GoToPara/Program.cs
@@ -9,27 +9,27 @@ namespace GoToPara
             Console.WriteLine("Желание пить пиво (введите целое число): ");
             int beer;
                 while (!int.TryParse(Console.ReadLine(), out beer))
-            {
+                {
                 Console.WriteLine("Ошибка ввода! Введите целое число ");
-            }
+                }
             Console.WriteLine("Желание спать (введите целое число): ");
             int sleep;
                 while (!int.TryParse(Console.ReadLine(), out sleep))
-            {
+                {
                 Console.WriteLine("Ошибка ввода! Введите целое число ");
-            }
+                }
             Console.WriteLine("Желание получить баллы (введите целое число): ");
             int points;
                 while (!int.TryParse(Console.ReadLine(), out points))
-            {
+                {
                 Console.WriteLine("Ошибка ввода! Введите целое число ");
-            }
+                }
             Console.WriteLine("Шанс отлететь на сессию (введите целое число): ");
             int death;
                 while (!int.TryParse(Console.ReadLine(), out death))
-            {
+                {
                 Console.WriteLine("Ошибка ввода! Введите целое число ");
-            }
+                }
 
             if (beer + sleep >= points + death)
             {
